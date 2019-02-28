9ce93f259a3a174d0d3759c265d27b31f503e71a
Karan Vohra <karan@kadenze.com>
2019-02-28 14:28:43 -0800

    added HTTPError checks to lyrics

--- testGettingLyrics.py
+++ testGettingLyrics.py
@@ -2,81 +2,45 @@ import urllib.request
 import ssl
 import re
 
-# def getLyrics(url): #lyrics.com
-# 	context = ssl._create_unverified_context()
-# 	with urllib.request.urlopen(url, context=context) as url:
-# 	    s = str(url.read())
 
-# 	lyricBodyIndex = s.find("lyric-body-text")
+class Lyrics():
 
-# 	if(lyricBodyIndex != -1):
-# 		lyricBodyStr = s[lyricBodyIndex:]
-# 		lyricBodyStr= lyricBodyStr[lyricBodyStr.find(">"):lyricBodyStr.find("</pre>")]
-# 		lyricBodyStr = re.sub("<(.|/n)*?>", "", lyricBodyStr)
+	def getLyrics(url):
+		context = ssl._create_unverified_context()
+		try:
+			conn = urllib.request.urlopen(url, context=context)
+		except urllib.error.HTTPError as e:
+			print('HTTPError: {}'.format(e.code))
+			return e.code
 
-# 		#is there a better solution to the replaces? Couldn't get regex to work for this...
-# 		lyricBodyStr = lyricBodyStr.replace("\\r\\n\\r\\n","@@").replace("\\r\\n","@@").replace(";",",").replace("- ","").replace("\\","")
-# 		lyricBodyStr = lyricBodyStr.split("@@")
-
-# 		lyricalBreakdown = []
-
-# 		for i in range(len(lyricBodyStr)):
-# 			lyricalBreakdown.append(re.sub(r'[^\w & ^\s]', '', lyricBodyStr[i]))
-
-# 		return(lyricalBreakdown)
+		except urllib.error.URLError as e:
+			print('URLError: {}'.format(e.reason))
+			return e.reason
+		else:
+			with urllib.request.urlopen(url, context=context) as url:
+				s = str(url.read())
+				return AZLyrics(s)
 
 
-# def createURL(): #This won't work, need to go to artist page and get url's from there...
-# 	context = ssl._create_unverified_context()
-# 	with urllib.request.urlopen("https://www.lyrics.com/artist/Chance-the-Rapper", context=context) as url:
-# 	    s = str(url.read())
+	def AZLyrics(s): #AZLyrics.com
+		lyricBodyStart = s.find("<!-- Usage of azlyrics.com")
+		lyricBody = s[lyricBodyStart:]
+		lyricBody = lyricBody[:lyricBody.find("</div>")]
+		lyricBody = re.sub("<(.|/n)*?>", "", lyricBody) #Remove HTML
+		lyricBody = re.sub("[\(\[].*?[\)\]]", "", lyricBody) #Remove intervals
+		lyricBody = lyricBody.replace("\\r\\n", "").replace("\\n\\n", "@@").replace("\\n", "@@").replace("\\", "").replace("&quot;", "\"").replace("...", " ") #remove sequences
+		lyricBody = lyricBody.split("@@")
 
-# 	print(s[s.find("/lyric/"):])
+		lyricBodyData = []
 
-# createURL()
+		for i in range(len(lyricBody)):
+			if(lyricBody[i] != ""):
+				lyricBodyData.append(re.sub(r'[^\w & ^\s]', '', lyricBody[i]).lower())
 
-#make one for azlyrics.com (easier)
+		return lyricBodyData
 
-def getLyrics(url):
-	context = ssl._create_unverified_context()
-	with urllib.request.urlopen(url, context=context) as url:
-	    s = str(url.read())
-	
-	if(s.find("It's a place where all searches end!") == -1):
-		return AZLyrics(s)
-	else:
-		return -1
-
-
-def AZLyrics(s): #AZLyrics.com
-	lyricBodyStart = s.find("<!-- Usage of azlyrics.com")
-	lyricBody = s[lyricBodyStart:]
-	lyricBody = lyricBody[:lyricBody.find("</div>")]
-	lyricBody = re.sub("<(.|/n)*?>", "", lyricBody) #Remove HTML
-	lyricBody = re.sub("[\(\[].*?[\)\]]", "", lyricBody) #Remove intervals
-	lyricBody = lyricBody.replace("\\r\\n", "").replace("\\n\\n", "@@").replace("\\n", "@@").replace("\\", "").replace("&quot;", "\"").replace("...", " ") #remove sequences
-	lyricBody = lyricBody.split("@@")
-
-	lyricBodyData = []
-
-	for i in range(len(lyricBody)):
-		if(lyricBody[i] != ""):
-			lyricBodyData.append(re.sub(r'[^\w & ^\s]', '', lyricBody[i]).lower())
-
-	return lyricBodyData
-
-def createAZLyricsUrl(artistName, songName):
-	return "https://www.azlyrics.com/lyrics/" + artistName.replace(" ", "") + "/" + songName.replace(" ", "") + ".html"
-
-
-def main():
-	url = createAZLyricsUrl("asap rocky", "been around the world")
-	lyrics = getLyrics(url)
-
-	for i in lyrics:
-		print(i)
-
-main()
+	def createAZLyricsUrl(artistName, songName):
+		return "https://www.azlyrics.com/lyrics/" + artistName.replace(" ", "") + "/" + songName.replace(" ", "") + ".html"
 
 
 
